feefa5a851859789fef34d31eec9db05ffa0ad38
Justin Heath <just.coder@outlook.com>
2024-08-15 12:53:16 +0100

    scaffold complete

---
+++ src/views/Homepage/page/HomePagePage.tsx
@@ -0,0 +1,7 @@
+import HomePageLayout from "../components/HomePageLayout/HomePageLayout";
+
+const HomePagePage: React.FC = (): JSX.Element => {
+  return <HomePageLayout />;
+};
+
+export default HomePagePage;
