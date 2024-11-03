98a0ec66ba807cb4b223f933c8ea78f0feb0facc
Justin Heath <just.coder@outlook.com>
2024-11-03 16:16:23 +0000

    homepage structure finished

--- src/common/styles/muiOverides.ts
+++ src/common/styles/muiOverides.ts
@@ -3,10 +3,7 @@ import { createTheme } from "@mui/material/styles";
 const themeOverides = createTheme({
   palette: {
     primary: {
-      main: "#fa8ab0",
-    },
-    secondary: {
-      main: "#b61588",
+      main: "#fc6621",
     },
   },
 });
--- src/views/Home/components/HomePageLayout/HomePageLayout.tsx
+++ src/views/Home/components/HomePageLayout/HomePageLayout.tsx
@@ -9,7 +9,7 @@ import "./HomePageLayout.scss";
 
 const HomePageLayout: React.FC = (): JSX.Element => {
   const warmUpDate = new Date("2025-04-28");
-  const eventDate = new Date("2025-09-28");
+  const eventDate = new Date("2025-08-11");
   const [warmUpTimeLeft, setWarmUpTimeLeft] = useState(calculateTimeLeft(warmUpDate));
   const [eventTimeLeft, setEventTimeLeft] = useState(calculateTimeLeft(eventDate));
   const [imageSrc, setImageSrc] = useState("");
@@ -49,15 +49,34 @@ const HomePageLayout: React.FC = (): JSX.Element => {
     <Stack alignItems={"center"} className="main-container">
       <img className="banner-image" src={imageSrc} alt="euros 2025 banner" />
       <Stack direction={"row"} justifyContent={"space-evenly"} className="cards-container">
-        <Card className="card-container">
+        <Card className="homepage-card">
           <Counter countDownData={{ ...warmUpTimeLeft, finishDate: warmUpDate }} />
         </Card>
-        <Card className="card-container">
+        <Card className="homepage-card">
           <Counter countDownData={{ ...eventTimeLeft, finishDate: eventDate }} />
         </Card>
       </Stack>
-      <Stack className="chips-container">
-        <Button>Test</Button>
+      <Stack direction={"row"} justifyContent={"space-evenly"} className="bottons-container">
+        <Button variant="contained" target="_blank" href="http://halifaxtrack.co.uk/" className="homepage-bottons">
+          Track
+        </Button>
+        {/* TODO - This link needs updating */}
+        <Button
+          variant="contained"
+          target="_blank"
+          href="https://www.efra.ws/race-calendar/"
+          className="homepage-bottons"
+        >
+          EFRA
+        </Button>
+        {/* TODO - This link needs updating */}
+        <Button variant="contained" target="_blank" href="www.google.com" className="homepage-bottons" disabled>
+          Live Timing
+        </Button>
+        {/* TODO - This link needs updating */}
+        <Button variant="contained" target="_blank" href="www.google.com" className="homepage-bottons" disabled>
+          Results
+        </Button>
       </Stack>
     </Stack>
   );
