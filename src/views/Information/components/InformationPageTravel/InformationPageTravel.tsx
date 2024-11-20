1c1061bac73c9c5746864be6b9514301af14e42e
Justin Heath <just.coder@outlook.com>
2024-11-20 22:03:21 +0000

    travel done

--- src/views/Information/components/InformationPageTravel/InformationPageTravel.tsx
+++ src/views/Information/components/InformationPageTravel/InformationPageTravel.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Link, Stack, Typography } from "@mui/material";
 import React from "react";
 
 import "./InformationPageTravel.scss";
@@ -7,6 +7,31 @@ const InformationPageTravel: React.FC = (): JSX.Element => {
   return (
     <Stack className="info-container">
       <Typography variant="h3">Travel</Typography>
+      <Typography variant="h5">Track Address</Typography>
+      <Typography>Halifax RC Race Track</Typography>
+      <Typography>Staniland Road</Typography>
+      <Typography>Halifax</Typography>
+      <Typography>HX4 8LS</Typography>
+      <Typography>United Kingdom</Typography>
+      <Typography variant="h5" sx={{ marginTop: "10px" }}>
+        Airports
+      </Typography>
+      <Typography>(cheap flights via Ryanair, Wizzair, easyjet & other airlines) </Typography>
+      <Typography>Closest airports:- Both around 45 minutes away </Typography>
+      <Typography>via car/also train links Leeds Bradford (22 miles) - recommended Manchester (43 miles)</Typography>
+      <Typography variant="h5" sx={{ marginTop: "10px" }}>
+        Boat (Ferry)
+      </Typography>
+      <Typography>Rotterdam to Hull is nearest</Typography>
+      <Typography>Other ports Zeebrugge Calais, Dunkirk to UK</Typography>
+      <Typography variant="h5" sx={{ marginTop: "10px" }}>
+        Tunnel
+      </Typography>
+      <Typography>Drive and use Train via euro tunnel only 35 minutes crossing</Typography>
+      <Typography>Calais (France) to Folkestone UK 276 miles to track</Typography>
+      <Link href="https://www.eurotunnel.com/uk/" target="_blank">
+        https://www.eurotunnel.com/uk/
+      </Link>
     </Stack>
   );
 };
